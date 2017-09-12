4b71761b45ad4348f6d0cd8ffda16955b037cebe
codeagles <hcn0521@outlook.com>
2017-09-12 15:53:21 +0800

    添加注释

--- src/com/maiget/dao/JedisCache.java
+++ src/com/maiget/dao/JedisCache.java
@@ -5,7 +5,9 @@ import redis.clients.jedis.Jedis;
 public class JedisCache {
 	
 	public static Jedis getJedis(String host){
+		
 		Jedis jedis = new Jedis(host);
+		//连接成功 则是PONG
 		if("PONG".equals(jedis.ping())){
 			return jedis;
 		}
--- src/common/CommonVar.java
+++ src/common/CommonVar.java
@@ -6,6 +6,7 @@ public class CommonVar {
 	
 	private Site site = Site.me().setDomain("blog.sina.com.cn").setSleepTime(3000).setUserAgent(
 			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.65 Safari/537.31");
+	//添加主机IP
 	public static final String HOST = "localhost";
 	public Site getSite() {
 		return site;
