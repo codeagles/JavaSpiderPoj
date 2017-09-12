44b5860a5e8498e345665318b37894d79dc906ac
codeagles <hcn0521@outlook.com>
2017-09-12 16:53:11 +0800

    捕捉jedis异常

--- src/com/maiget/dao/JedisCache.java
+++ src/com/maiget/dao/JedisCache.java
@@ -5,8 +5,14 @@ import redis.clients.jedis.Jedis;
 public class JedisCache {
 	
 	public static Jedis getJedis(String host){
-		
-		Jedis jedis = new Jedis(host);
+		Jedis jedis =null;
+	
+		try{
+			jedis = new Jedis(host);
+		}catch(Exception e){
+			e.printStackTrace();
+			System.out.println("Jedis拒绝访问："+e);
+		}
 		//连接成功 则是PONG
 		if("PONG".equals(jedis.ping())){
 			System.out.println("获得Jedis对象");
