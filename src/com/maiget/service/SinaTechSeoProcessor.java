0da3687be8621b3b539f1227cf93d2bfc787b81e
codeagles <hcn0521@outlook.com>
2017-12-25 13:38:02 +0800

    修复新浪IT目录爬取规则

--- src/com/maiget/service/SinaTechSeoProcessor.java
+++ src/com/maiget/service/SinaTechSeoProcessor.java
@@ -7,9 +7,12 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -25,21 +28,29 @@ public class SinaTechSeoProcessor implements PageProcessor {
 
 	@Override
 	public void process(Page page) {
+		System.out.println("开始爬取链接");
 		if (page.getUrl().regex(ENTRYURL).match()) {
 			urlLists = page.getHtml().xpath("//ul[@class=\"seo_data_list\"]").links().regex(POSTURL).all();
 			page.addTargetRequests(urlLists);
 		} else {
-			if (!(page.getHtml().xpath("//h1[@id=\"main_title\"]/text()").get().toString().isEmpty())
-					&& !(page.getHtml().xpath("//a[@class =\'ent1 fred\']/text()").get().toString().isEmpty())) {
+			String author = page.getHtml().xpath("//a[@class =\'source ent-source\']/text()").get();
+			String title = page.getHtml().xpath("//h1/text()").get().toString();
+			if("".equals(author) || null == author){
+				author = page.getHtml().xpath("//span[@class =\'source\']/text()").get();
+			}
+			if (!(title.isEmpty())
+					&& !(author.isEmpty())) {
 				NewsBean bean = new NewsBean();
-				bean.setTitle(page.getHtml().xpath("//h1[@id=\"main_title\"]/text()").get());
-				bean.setAuthor(page.getHtml().xpath("//a[@class =\'ent1 fred\']/text()").get());
-				bean.setNewstime(page.getHtml().xpath("//span[@class =\'titer\']/text()").get());
+				bean.setTitle(page.getHtml().xpath("//h1/text()").get());
+				bean.setAuthor(author);
+				bean.setNewstime(page.getHtml().xpath("//span[@class =\'date\']/text()").get());
 				bean.setCategory("科技");
 				bean.setOrigin("新浪科技");
 				bean.setContent(page.getHtml().xpath("//div[@id=\'artibody\']").get());
 				String imgUrl = page.getHtml().xpath("//div[@id=\'artibody\']/div[@class =\'img_wrapper\']").css("img", "src").get();
 				bean.setImg(imgUrl);
+				SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:ss:mm");
+				bean.setCreatetime(sdf.format(new Date()));
 				MDao mDao = new MDao();
 				int i = mDao.addInfo(bean);
 				if (i > 0) {
@@ -58,9 +69,9 @@ public class SinaTechSeoProcessor implements PageProcessor {
 		return new CommonVar().getSite();
 	}
 
-//	public static void main(String[] args) {
-//		Spider.create(new SinaTechProcessor()).addUrl("http://tech.sina.com.cn").thread(10).run();
-//		System.out.println("爬取结束");
-//	}
+	public static void main(String[] args) {
+		Spider.create(new SinaTechSeoProcessor()).addUrl("http://tech.sina.com.cn").thread(5).run();
+		System.out.println("爬取结束");
+	}
 
 }
