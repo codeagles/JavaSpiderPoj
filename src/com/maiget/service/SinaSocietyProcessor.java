cdd26b95806c5decbea9d100179ff39fd0310646
codeagles <hcn0521@outlook.com>
2017-12-25 18:05:02 +0800

    修复社会分类爬虫

--- src/com/maiget/service/SinaSocietyProcessor.java
+++ src/com/maiget/service/SinaSocietyProcessor.java
@@ -1,24 +1,19 @@
 package com.maiget.service;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-
-import org.apache.commons.collections.map.HashedMap;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import com.maiget.dao.JedisCache;
 import com.maiget.dao.MDao;
 import com.maiget.model.NewsBean;
-
 import common.CommonVar;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import redis.clients.jedis.Jedis;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
-import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 import util.MD5Util;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 
@@ -32,11 +27,11 @@ public class SinaSocietyProcessor implements PageProcessor {
 	private static String ENTRYURL = "http://news.sina.com.cn/society/";
 	private Jedis jedis = JedisCache.getJedis(CommonVar.HOST);
 
-	// public static void main(String[] args) {
-	// Spider.create(new
-	// SinaSocietyProcessor()).addUrl(ENTRYURL).thread(5).run();
-	// System.out.println("爬取结束");
-	// }
+	 public static void main(String[] args) {
+//		 Spider.create(new
+//		 SinaSocietyProcessor()).addUrl(ENTRYURL).thread(5).run();
+//		 System.out.println("爬取结束");
+	 }
 
 	@Override
 	public void process(Page page) {
@@ -47,9 +42,9 @@ public class SinaSocietyProcessor implements PageProcessor {
 		} else {
 			//判断
 			MDao mDao = new MDao();
-			String title = page.getHtml().xpath("//h1[@id=\"artibodyTitle\"]/text()").get().toString();
-			String author = page.getHtml().xpath("//span[@data-sudaclick =\'media_name\']/a/text()").get().toString();
-			String titlemd5 = MD5Util.md5Password(title);
+			String title = page.getHtml().xpath("//h1[@class=\"main-title\"]/text()").get().toString();
+			String author = page.getHtml().xpath("//a[@class =\'source\']/text()").get().toString();
+			String titlemd5 = MD5Util.md5Str(title);
 			if(!jedis.sismember("md5title", titlemd5)){
 				jedis.sadd("md5title", titlemd5);
 				System.out.println("redis written");
@@ -57,11 +52,11 @@ public class SinaSocietyProcessor implements PageProcessor {
 					NewsBean bean = new NewsBean();
 					bean.setTitle(title);
 					bean.setAuthor(author);
-					bean.setNewstime(page.getHtml().xpath("//span[@class =\'time-source\']/text()").get());
+					bean.setNewstime(page.getHtml().xpath("//span[@class =\'date\']/text()").get());
 					bean.setCategory("社会");
 					bean.setOrigin("新浪新闻中心");
-					bean.setContent(page.getHtml().xpath("//div[@id=\'artibody\']").get());
-					String imgUrl = page.getHtml().xpath("//div[@id=\'artibody\']/div[@class =\'img_wrapper\']")
+					bean.setContent(page.getHtml().xpath("//div[@id=\'article\']").get());
+					String imgUrl = page.getHtml().xpath("//div[@id=\'article\']/div[@class =\'img_wrapper\']")
 							.css("img", "src").get();
 					bean.setImg(imgUrl);
 
