dcfcebb9566081b10da5a4585d56f2f343d90fb4
codeagles <hcn0521@outlook.com>
2017-12-25 15:18:22 +0800

    增加redis去重

--- src/com/maiget/service/SinaEntProcessor.java
+++ src/com/maiget/service/SinaEntProcessor.java
@@ -1,24 +1,22 @@
 package com.maiget.service;
 
-import java.sql.Connection;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
 import com.maiget.dao.JedisCache;
 import com.maiget.dao.MDao;
 import com.maiget.model.NewsBean;
-
 import common.CommonVar;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import redis.clients.jedis.Jedis;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
 import util.MD5Util;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class SinaEntProcessor implements PageProcessor {
 	private static Log logger = LogFactory.getLog(SinaEntProcessor.class);
@@ -29,37 +27,38 @@ public class SinaEntProcessor implements PageProcessor {
 	private Jedis jedis = JedisCache.getJedis(CommonVar.HOST);
 
 	
-//	public static void main(String[] args) {
-//		Spider.create(new SinaEntProcessor()).addUrl(ENTRYURL).thread(1).run();
-//		System.out.println("爬取结束");
-//	}
+	public static void main(String[] args) {
+		Spider.create(new SinaEntProcessor()).addUrl(ENTRYURL).thread(1).run();
+		System.out.println("爬取结束");
+	}
 
 	@Override
 	public void process(Page page) {
-		MDao mDao = new MDao();
+
 		if (page.getUrl().regex(ENTRYREGURL).match()) {
 			urlLists = page.getHtml().xpath("//ul[@class=\'module_common_list clearfix\']").links().regex(POSTURL)
 					.all();
 			page.addTargetRequests(urlLists);
 		} else {
-			String title = page.getHtml().xpath("//h1[@id=\"main_title\"]/text()").get().toString();
-			String author = page.getHtml().xpath("//a[@data-sudaclick =\'media_name\']/text()").get().toString();
-			String titlemd5 = MD5Util.md5Password(title);
+			String title = page.getHtml().xpath("//h1[@class=\"main-title\"]/text()").get();
+			String author = page.getHtml().xpath("//a[@data-sudaclick =\'content_media_p\']/text()").get();
+			String titlemd5 = MD5Util.md5Str(title);
 			if(!jedis.sismember("md5title", titlemd5)){
 				jedis.sadd("md5title", titlemd5);
-				System.out.println("redis written");
 				if (!(title.isEmpty()) && !(author.isEmpty())) {
 					NewsBean bean = new NewsBean();
 					bean.setTitle(title);
 					bean.setAuthor(author);
-					bean.setNewstime(page.getHtml().xpath("//span[@class =\'titer\']/text()").get());
+					bean.setNewstime(page.getHtml().xpath("//span[@class =\'date\']/text()").get());
 					bean.setCategory("娱乐");
 					bean.setOrigin("新浪娱乐");
 					bean.setContent(page.getHtml().xpath("//div[@id=\'artibody\']").get());
 					String imgUrl = page.getHtml().xpath("//div[@id=\'artibody\']/div[@class =\'img_wrapper\']")
 							.css("img", "src").get();
 					bean.setImg(imgUrl);
-
+					SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:ss:mm");
+					bean.setCreatetime(sdf.format(new Date()));
+					MDao mDao = new MDao();
 					int i = mDao.addInfo(bean);
 					if (i > 0) {
 						System.out.println("insert successed！");
