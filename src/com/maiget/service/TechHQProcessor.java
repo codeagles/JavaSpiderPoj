c71afce49fee2ced67a48ae0ef6d89740b2f037e
codeagles <hcn0521@outlook.com>
2017-12-26 13:49:26 +0800

    修复环球网爬虫

--- src/com/maiget/service/TechHQProcessor.java
+++ src/com/maiget/service/TechHQProcessor.java
@@ -1,51 +1,69 @@
 package com.maiget.service;
 
+import com.maiget.dao.JedisCache;
 import com.maiget.dao.MDao;
 import com.maiget.model.NewsBean;
+import common.CommonVar;
+import redis.clients.jedis.Jedis;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
 import us.codecraft.webmagic.processor.PageProcessor;
+import util.MD5Util;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
-public class TechHQProcessor implements PageProcessor{
-	
-	private String POSTURL ="http://tech\\.huanqiu\\.com/.*\\.html";
-	private List<String> urlList = new ArrayList<String>();
-	private MDao mdao = new MDao();
-	private Site site = Site.me().setSleepTime(3000).setUserAgent(
-			"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.65 Safari/537.31");
-	
-	@Override
-	public Site getSite() {
-		return site;
-	}
+public class TechHQProcessor implements PageProcessor {
 
-	@Override
-	public void process(Page page) {
-		
-		if(!page.getUrl().regex(POSTURL).match()){
-			urlList=page.getHtml().xpath("//div[@id=\"active1Con\"]").links().all();
-			page.addTargetRequests(urlList);
-		}else{
-			NewsBean bean = new NewsBean();
-			bean.setTitle(page.getHtml().xpath("//h1/text()").get());
-			bean.setAuthor("环球科技");
-			bean.setOrigin("环球科技");
-			bean.setCategory("科技");
-			bean.setContent(page.getHtml().xpath("//div[@id=\'text\']").get());
-			bean.setNewstime(page.getHtml().xpath("//strong[@class=\"timeSummary\"]/text()").get());
-			bean.setImg("");
-			int i = mdao.addInfo(bean);
-			if(i>0){
-				System.out.println("插入成功");
-			}
-		}
+    private String POSTURL = "http://tech\\.huanqiu\\.com/.*\\.html";
+    private List<String> urlList = new ArrayList<String>();
+    private MDao mdao = new MDao();
+    private Site site = Site.me().setSleepTime(3000).setUserAgent(
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_2) AppleWebKit/537.31 (KHTML, like Gecko) Chrome/26.0.1410.65 Safari/537.31");
+    private Jedis jedis = JedisCache.getJedis(CommonVar.HOST);
 
-	}
-//	public static void main(String[] args) {
-//		Spider.create(new TechHQProcessor()).addUrl("http://tech.huanqiu.com").run();
-//	}
-	
+    @Override
+    public Site getSite() {
+        return site;
+    }
+
+    @Override
+    public void process(Page page) {
+
+        if (!page.getUrl().regex(POSTURL).match()) {
+            urlList = page.getHtml().xpath("//div[@id=\"active1Con\"]").links().all();
+            page.addTargetRequests(urlList);
+        } else {
+            NewsBean bean = new NewsBean();
+            String title = page.getHtml().xpath("//h1/text()").get();
+            String titlemd5 = MD5Util.md5Str(title);
+            if (!jedis.sismember("md5title", titlemd5)) {//添加redis set集合去重
+                jedis.sadd("md5title", titlemd5);//不存在则添加
+                if (!title.isEmpty()) {
+                    bean.setTitle(title);
+                    bean.setAuthor("环球科技");
+                    bean.setOrigin("环球科技");
+                    bean.setCategory("科技");
+                    bean.setContent(page.getHtml().xpath("//div[@id=\'text\']").get());
+                    bean.setNewstime(page.getHtml().xpath("//strong[@class=\"timeSummary\"]/text()").get());
+                    String imgUrl = page.getHtml().xpath("//div[@id=\'text\']")
+                            .css("img", "src").get();
+                    SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:ss:mm");
+                    bean.setCreatetime(sdf.format(new Date()));
+                    bean.setImg(imgUrl);
+                    int i = mdao.addInfo(bean);
+                    if (i > 0) {
+                        System.out.println("插入成功");
+                    }
+                }
+            }
+        }
+    }
+
+//    public static void main(String[] args) {
+//        Spider.create(new TechHQProcessor()).addUrl("http://tech.huanqiu.com").run();
+//    }
+
 }
--- src/com/maiget/service/TencentTechProcessor.java
+++ src/com/maiget/service/TencentTechProcessor.java
@@ -1,13 +1,19 @@
 package com.maiget.service;
 
+import com.maiget.dao.JedisCache;
 import com.maiget.dao.MDao;
 import com.maiget.model.NewsBean;
 import common.CommonVar;
+import redis.clients.jedis.Jedis;
 import us.codecraft.webmagic.Page;
 import us.codecraft.webmagic.Site;
+import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.processor.PageProcessor;
+import util.MD5Util;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class TencentTechProcessor implements PageProcessor{
@@ -15,7 +21,7 @@ public class TencentTechProcessor implements PageProcessor{
 	private String ENTRYREGURL = "http://tech\\.qq\\.com$";
 	private List<String> urlLists = new ArrayList<>();
 	private String POSTURL = "http://tech\\.qq\\.com/a/.*.htm";
-//	private Jedis jedis = JedisCache.getJedis(CommonVar.HOST);
+	private Jedis jedis = JedisCache.getJedis(CommonVar.HOST);
 	
 	@Override
 	public void process(Page page) {
@@ -26,28 +32,33 @@ public class TencentTechProcessor implements PageProcessor{
 		}else{
 			MDao mdao = new MDao();
 			String title = page.getHtml().xpath("//h1/text()").get().toString();
+			String titlemd5 = MD5Util.md5Str(title);
 			String author = page.getHtml().xpath("//span[@class=\'a_source\']/text()").get();
 			String newstime = page.getHtml().xpath("//span[@class=\'a_time\']/text()").get();
 			String content = page.getHtml().xpath("//div[@class=\'Cnt-Main-Article-QQ\']").get();
 			String img = page.getHtml().xpath("//div[@class=\'Cnt-Main-Article-QQ\']/p").css("img", "src").get();
-			if(!title.isEmpty()&&!author.isEmpty()){
-				NewsBean bean = new NewsBean();
-				bean.setAuthor(author);
-				bean.setTitle(title);
-				bean.setCategory("科技");
-				bean.setOrigin("腾讯科技");
-				bean.setNewstime(newstime);
-				bean.setImg(img);
-				bean.setContent(content);
-				int i = mdao.addInfo(bean);
-				if (i > 0) {
-					System.out.println("insert successed！");
-				} else {
-					System.out.println("insert failed!");
+			if(!jedis.sismember("md5title", titlemd5)) {//添加redis set集合去重
+				jedis.sadd("md5title", titlemd5);//不存在则添加
+				if (!title.isEmpty() && !author.isEmpty()) {
+					NewsBean bean = new NewsBean();
+					bean.setAuthor(author);
+					bean.setTitle(title);
+					bean.setCategory("科技");
+					bean.setOrigin("腾讯科技");
+					bean.setNewstime(newstime);
+					bean.setImg(img);
+					bean.setContent(content);
+					SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:ss:mm");
+					bean.setCreatetime(sdf.format(new Date()));
+					int i = mdao.addInfo(bean);
+					if (i > 0) {
+						System.out.println("insert successed！");
+					} else {
+						System.out.println("insert failed!");
+					}
 				}
+
 			}
-			
-			
 		}
 	}
 
@@ -55,10 +66,10 @@ public class TencentTechProcessor implements PageProcessor{
 	public Site getSite() {
 		return new CommonVar().getSite();
 	}
-	
-	
-//	public static void main(String[] args) {
-//		Spider.create(new TencentTechProcessor()).addUrl(ENTRYURL).run();
-//		System.out.println("爬取结束");
-//	}
+
+
+	public static void main(String[] args) {
+		Spider.create(new TencentTechProcessor()).addUrl(ENTRYURL).run();
+		System.out.println("爬取结束");
+	}
 }
